54d7ec98f64d4e695985a0df75f6b3c728348ee0
Dustin <dmkelly100@gmail.com>
2020-09-18 12:26:59 -0400

    Dashboard setup

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -9,6 +9,7 @@ import {
 	LOGIN_FAIL,
 	LOGIN_SUCCESS,
 	LOGOUT,
+	CLEAR_PROFILE,
 } from './types';
 
 // Load User
@@ -92,5 +93,7 @@ export const login = (email, password) => async (dispatch) => {
 // Logout / Clear Profile
 
 export const logout = () => (dispatch) => {
+	dispatch({ type: CLEAR_PROFILE });
 	dispatch({ type: LOGOUT });
+
 };
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,15 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
 	const authLinks = (
 		<ul>
 			<li>
+				<Link to='/dashboard'>
+					{' '}
+					<i className='fas fa-user' />{' '}
+					<span className='hide-sm'> Dashboard</span>
+				</Link>
+			</li>
+			<li>
 				<Link onClick={logout} to='/login'>
-					<i className='fas fa-sign-out-alt"'></i>
+					<i className='fas fa-sign-out-alt' />{' '}
 					<span className='hide-sm'> Logout</span>
 				</Link>
 			</li>
