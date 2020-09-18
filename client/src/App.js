e3b8405518ab724a6e33f591c8d3fde0cda70c82
Dustin <dmkelly100@gmail.com>
2020-09-17 20:46:12 -0400

    added route for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,10 @@ import Landing from './components/layout/Landing';
 import Alert from './components/layout/Alert';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import {loadUser} from './actions/auth';
-import setAuthToken from './utils/setAuthToken'
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
@@ -16,9 +18,9 @@ if (localStorage.token) {
 }
 
 const App = () => {
-	useEffect(()=> {
-		store.dispatch(loadUser())
-	},[])
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
 	return (
 		<Provider store={store}>
 			{' '}
@@ -32,6 +34,7 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
 						</Switch>
 					</section>
 				</Fragment>
