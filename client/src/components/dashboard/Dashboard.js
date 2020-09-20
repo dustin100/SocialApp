fd8ed58c58d6d08a3692a9a45dd55e48ecfcdd92
Dustin <dmkelly100@gmail.com>
2020-09-19 21:08:37 -0400

    created display components for education and experience

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import Experience from './Experience';
+import Education from './Education';
+
 import DashboardActions from './DashboardActions';
 import { getCurrentProfile } from '../../actions/profile';
 
@@ -25,6 +28,8 @@ const Dashboard = ({
 			{profile !== null ? (
 				<Fragment>
 					<DashboardActions />
+					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
 				</Fragment>
 			) : (
 				<Fragment>
