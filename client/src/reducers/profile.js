9e8c345242cd1c1a98a72c85fe19debacf33d601
Dustin <dmkelly100@gmail.com>
2020-09-20 11:49:17 -0400

    updated actions and reducers

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,8 +1,10 @@
 import {
 	PROFILE_ERROR,
 	GET_PROFILE,
+	GET_PROFILES,
 	CLEAR_PROFILE,
 	UPDATE_PROFILE,
+	GET_REPOS,
 } from '../actions/types';
 
 const initialState = {
@@ -24,7 +26,12 @@ export default function (state = initialState, action) {
 				profile: payload,
 				loading: false,
 			};
-
+		case GET_PROFILES:
+			return {
+				...state,
+				profiles: payload,
+				loading: false,
+			};
 		case PROFILE_ERROR:
 			return {
 				...state,
@@ -38,6 +45,13 @@ export default function (state = initialState, action) {
 				repos: [],
 				loading: false,
 			};
+		case GET_REPOS:
+			return {
+				...state,
+				repos: payload,
+				loading: false,
+			};
+
 		default:
 			return state;
 	}
